09e6a7b4d8681269d63f92765acf7702ca870a3c
NakagawaYuki <kingwasheart@yahoo.co.jp>
2018-05-20 02:18:11 +0900

    ファーストコミット 目的：探せばありそうだがXinput直書きで扱えるようにしたくなったので作ることにした

---
+++ XinputInit/XinputInit/main.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+#include <Xinput.h>
+#include <stdio.h>
+
+int main()
+{
+	/*XINPUT_STATE input;
+	input.dwPacketNumber = XInputGetState(0, &input);
+
+	printf("%d", input.Gamepad.bLeftTrigger);*/
+
+	return 0;
+}
