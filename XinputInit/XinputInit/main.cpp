4e7bdee501f86add65150df41f7ba74118f5833c
NakagawaYuki <kingwasheart@yahoo.co.jp>
2018-05-20 11:23:03 +0900

    xinputのAボタンのみ対応、デバッグしづらいのでコンソールから変えるかも gitignore調整、.vsファイルを無効化

--- XinputInit/XinputInit/main.cpp
+++ XinputInit/XinputInit/main.cpp
@@ -2,12 +2,34 @@
 #include <Xinput.h>
 #include <stdio.h>
 
+#pragma comment(lib, "xinput.lib")
+
+#define MAX_CONTROLLERS 4
+
 int main()
 {
-	/*XINPUT_STATE input;
-	input.dwPacketNumber = XInputGetState(0, &input);
-
-	printf("%d", input.Gamepad.bLeftTrigger);*/
-
+	int test = 0;
+	for (DWORD i = 0; i< MAX_CONTROLLERS; i++) 
+	{
+		XINPUT_STATE state[MAX_CONTROLLERS];
+		ZeroMemory(&state[i], sizeof(XINPUT_STATE)); 
+		if(XInputGetState(i, &state[i]) == ERROR_SUCCESS )
+		{      
+			// Œq‚ª‚Á‚½ 
+			if ((state[i].Gamepad.wButtons & XINPUT_GAMEPAD_A) != 0)
+			{
+				test++;
+			}
+			else
+			{
+				test = 0;
+			}
+		}    
+		else 
+		{        
+			// Œq‚ª‚Á‚Ä‚¢‚È‚¢ 
+		}
+	}
+	printf("%d", test);
 	return 0;
 }
