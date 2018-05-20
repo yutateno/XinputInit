3109b9ead396ba9c2b19666f240e8b00fa802e33
NakagawaYuki <kingwasheart@yahoo.co.jp>
2018-05-20 12:30:48 +0900

    xButton関連のみ対応した、長押し対応できてる

--- XinputInit/XinputInit/main.cpp
+++ XinputInit/XinputInit/main.cpp
@@ -1,35 +1,47 @@
 #include <Windows.h>
 #include <Xinput.h>
 #include <stdio.h>
+#include <math.h>
+#include <winerror.h>
 
 #pragma comment(lib, "xinput.lib")
 
-#define MAX_CONTROLLERS 4
+const static int MAX_CONTROLLERS = 4;
 
 int main()
 {
-	int test = 0;
-	for (DWORD i = 0; i< MAX_CONTROLLERS; i++) 
+	int test[16]{ 0,0,0,0,0,0,0,0,0,0,-1,-1,0,0,0,0};
+	while (true)
 	{
-		XINPUT_STATE state[MAX_CONTROLLERS];
-		ZeroMemory(&state[i], sizeof(XINPUT_STATE)); 
-		if(XInputGetState(i, &state[i]) == ERROR_SUCCESS )
-		{      
-			// Œq‚ª‚Á‚½ 
-			if ((state[i].Gamepad.wButtons & XINPUT_GAMEPAD_A) != 0)
+		for (DWORD i = 0; i < MAX_CONTROLLERS; i++)
+		{
+			XINPUT_STATE state[MAX_CONTROLLERS];
+			ZeroMemory(&state[i], sizeof(XINPUT_STATE));
+			if (XInputGetState(i, &state[i]) == ERROR_SUCCESS)
 			{
-				test++;
+				// ƒ{ƒ^ƒ“‘€ì
+				for (int j = 0; j != 16; ++j)
+				{
+					if (j == 10 || j == 11)
+					{
+						continue;
+					}
+					if ((state[i].Gamepad.wButtons & (int)pow(2.0, (double)j)) != 0)
+					{
+						test[j]++;
+					}
+					else
+					{
+						test[j] = 0;
+					}
+					printf("%d", test[j]);
+				}
 			}
 			else
 			{
-				test = 0;
+				ZeroMemory(&state[i], sizeof(XINPUT_STATE));
 			}
-		}    
-		else 
-		{        
-			// Œq‚ª‚Á‚Ä‚¢‚È‚¢ 
 		}
 	}
-	printf("%d", test);
 	return 0;
 }
